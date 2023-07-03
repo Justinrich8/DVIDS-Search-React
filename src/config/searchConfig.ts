18362e3415f7ab9ff9655cdc0435db1bbcc1b5c8
Justinrich8 <109759966+Justinrich8@users.noreply.github.com>
2023-07-03 09:41:38 -0400

    vertical updates

--- src/config/searchConfig.ts
+++ src/config/searchConfig.ts
@@ -1,6 +1,6 @@
 export const config = {
   apiKey: "3f823ea1c87c6154d6c069c8f77c4b51",
-  experienceKey: "dvids-search  ",
+  experienceKey: "dvids-search",
   locale: "en",
   experienceVersion: "STAGING",
   businessId: 3217762,
